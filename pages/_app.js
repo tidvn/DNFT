eb51becfea458d9008827876dde488d9b88ad19a
tidvn <tiendung0325@gmail.com>
2023-04-20 17:08:26 +0700

    update txt

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { MeshProvider } from "@meshsdk/react";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <MeshProvider>
+      <Head>
+        <title>Mint NFT in Cardano</title>
+        <link rel="icon" type="image/png" href="/icon.png"/>
+      </Head>
       <div className="container mx-auto">
       <Component {...pageProps} />
       </div>
